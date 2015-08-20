0486cd3b31f37cad7ac6eaa5c462d6878d97c7ab
Sunit15 <titima@pbru.ac.th>
2015-08-20 15:57:43 +0700

    clound

--- core/src/appPbru/sunit/Thai/MyGdxGame.java
+++ core/src/appPbru/sunit/Thai/MyGdxGame.java
@@ -13,9 +13,11 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	//Explicit
 	private SpriteBatch batch;
-	private Texture wallpaperTexture;
+	private Texture wallpaperTexture, cloudTexture;
 	private OrthographicCamera objOrthographicCamera; //ปรับ scale ให้ตามขนาดจอ
 	private BitmapFont nameBitmapFont;
+	private int xCloundAnInt, yCloundAnInt = 600;
+	private boolean cloundABoolean = true;
 
 
 	@Override
@@ -32,6 +34,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		nameBitmapFont = new BitmapFont();
 		nameBitmapFont.setColor(Color.WHITE);
 		nameBitmapFont.setScale(4);
+
+		//Setup clound
+		cloudTexture = new Texture("cloud.png");
 
 
 	}  //create เอาไว้กำหนดค่า
@@ -50,9 +55,35 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Drawable Wallpaper
 		batch.draw(wallpaperTexture, 0, 0);
 
+		//drawable clound
+		batch.draw(cloudTexture, xCloundAnInt,yCloundAnInt);
+
 		//DrawableBitMapFont
 		nameBitmapFont.draw(batch, "Coins PBRU", 50, 600);
 
 		batch.end();
+
+		//Move clound
+		moveClound();
+
 	}    //render ไว้วนลูป
+
+	private void moveClound() {
+
+		if (cloundABoolean) {
+			if (xCloundAnInt < 937) {
+				xCloundAnInt += 100 * Gdx.graphics.getDeltaTime();
+			} else {
+				cloundABoolean = !cloundABoolean;
+			}
+		} else {
+			if (xCloundAnInt > 0) {
+				xCloundAnInt -= 100 * Gdx.graphics.getDeltaTime();
+			} else {
+				cloundABoolean = !cloundABoolean;
+			}
+
+		}
+
+	}	//move clound
 }    //main class
